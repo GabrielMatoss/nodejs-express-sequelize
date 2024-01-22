46517a4ebea64358e9e7150002837455788ba900
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-22 11:05:37 -0300

    feat(pessoacontroller): criando a classe PessoaController

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async pegaTodas(req, res) {
+    try {
+    //codigo que acessa o modelo
+    } catch (error) {
+      //tratamento de erro      
+    }
+  }
+}
+
+module.exports = PessoaController;
