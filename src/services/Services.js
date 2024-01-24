af2b41f23b2aa0ffbaa7b87afe615202ef0a32b4
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-23 21:43:21 -0300

    feat(services): criação da camada services para desacoplar o model do controller

---
+++ src/services/Services.js
@@ -0,0 +1,14 @@
+const dataSource = require('../models');
+
+class Services {
+  constructor(nomeDoModel) {
+    this.model = nomeDoModel;
+  }
+
+  async pegaTodosOsRegistros () {
+    return dataSource[this.model].findAll();
+  }
+}
+
+
+module.exports = Services;
