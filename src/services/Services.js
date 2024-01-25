6ccb6bc0d16efde18140ebf4d6bf2fd0d5899e34
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-25 14:46:44 -0300

    feat(services.js): criação do services que atualiza o banco de dados

--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,17 @@ class Services {
   async pegaTodosOsRegistros () {
     return dataSource[this.model].findAll();
   }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listaDeRegistrosAtualizados = await dataSource[this.model].update(dadosAtualizados, {
+      where: {id: id}
+    });
+    
+    if (listaDeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
 }
 
 
