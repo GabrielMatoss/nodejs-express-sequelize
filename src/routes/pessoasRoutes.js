a7d2d79eff242b5b88369f7ab17b5ba5e69db81d
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-25 14:50:29 -0300

    feat(pessoasroutes.js): criação da rota put

--- src/routes/pessoasRoutes.js
+++ src/routes/pessoasRoutes.js
@@ -5,5 +5,6 @@ const pessoaController = new PessoaController();
 const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
 
 module.exports = router;
