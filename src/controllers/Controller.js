e756bde6aeac31211cadb34be27d3afd39b357e2
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-25 14:49:19 -0300

    feat(controller.js): criação do método atualiza, que nos retorna a resposta http

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -11,6 +11,24 @@ class Controller {
       //tratamento de erro      
     }
   }
+
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+
+    try {
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+
+      if (!foiAtualizado) {
+        return res.status(400).json({message: 'Registro não foi atualizado'});
+      }
+      return res.status(200).json({message: 'Atualizado com sucesso'});
+
+    } catch (error) {
+      //error
+    }
+  }
+  
 }
 
 module.exports = Controller;
