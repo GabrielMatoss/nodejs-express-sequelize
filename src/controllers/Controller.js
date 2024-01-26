a7c836bbbb202bd297b3f2017f8523e59ac6628b
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-26 10:14:58 -0300

    feat(controller): criação do método pegaUmPorId
    Esse método utiliza o service pegaUmRegistroPorId, e retorna mensagens http, esse service também foi
    usado para retonar o registro atualizado quando fazemos uma operação com método put

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -12,23 +12,61 @@ class Controller {
     }
   }
 
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      
+      if(!umRegistro) {
+        return res.status(400).json({message: 'Registro não foi encontrado'});
+      }
+
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  // async criaNovo(req, res) {
+  //   const dadosParaCriacao = req.body;
+  //   try {
+  //     const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+  //     return res.status(200).json(novoRegistroCriado);
+  //   } catch (erro) {
+  //     // erro
+  //   }
+  // }
+
   async atualiza(req, res) {
     const { id } = req.params;
     const dadosAtualizados = req.body;
 
     try {
       const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      
 
-      if (!foiAtualizado) {
+      if (!foiAtualizado && !umRegistro) {
         return res.status(400).json({message: 'Registro não foi atualizado'});
       }
-      return res.status(200).json({message: 'Atualizado com sucesso'});
+      return res.status(200).json({message: 'Atualizado com sucesso', registroModificado: umRegistro});
 
     } catch (error) {
       //error
     }
   }
-  
+
+  // async exclui(req, res) {
+  //   const { id } = req.params;
+  //   try {
+  //     await this.entidadeService.excluiRegistro(Number(id));
+  //     return res.status(200).json({ mensagem: `id ${id} deletado` });
+
+
+  //   } catch (error) {
+  //     return res.status(500).json(error.message);
+  //   }
+  // }
 }
 
 module.exports = Controller;
