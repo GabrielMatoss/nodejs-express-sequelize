9e80f9fdd154136ac673ef836a2e750878013ced
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-26 10:11:13 -0300

    feat(services): criando método que pega um registro por Pk e o retorna

--- src/services/Services.js
+++ src/services/Services.js
@@ -9,16 +9,34 @@ class Services {
     return dataSource[this.model].findAll();
   }
 
+  async pegaUmRegistroPorId(id) {
+    const registro = await dataSource[this.model].findByPk(id);
+
+    if (registro === null) {
+      return false;
+    } 
+
+    return registro;
+  }
+
+  // async criaRegistro(dadosDoRegistro) {
+  //   return dataSource[this.model].create(dadosDoRegistro);
+  // }
+
   async atualizaRegistro(dadosAtualizados, id) {
     const listaDeRegistrosAtualizados = await dataSource[this.model].update(dadosAtualizados, {
       where: {id: id}
     });
-    
+
     if (listaDeRegistrosAtualizados[0] === 0) {
       return false;
     }
     return true;
   }
+
+  // async excluiRegistro(id) {
+  //   return dataSource[this.model].destroy({ where: { id: id } });
+  // }
 }
 
 
