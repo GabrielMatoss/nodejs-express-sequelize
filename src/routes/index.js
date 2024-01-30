e18a5774add77db0220ca112f64bdcee0d2781b3
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-01-30 10:19:20 -0300

    feat: adicionando os routers de categoria e cursos no middleware index das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const pessoasRouter = require('./pessoasRoutes.js');
+const categoriasRouter = require('./categoriasRoutes.js');
+const cursosRouter = require('./cursosRoutes.js');
 
 module.exports = (app) => {
   app.use(
     express.json(),
-    pessoasRouter
+    pessoasRouter,
+    categoriasRouter,
+    cursosRouter
   );
 };
