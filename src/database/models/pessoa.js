7d1b0eafd54d3c942ce8d20f1016bc0555308096
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-02-15 17:50:45 -0300

    feat(models): adiciona scopes ao model de pessoas

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -26,6 +26,16 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Pessoa',
     tableName: 'pessoas',
     paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true,
+      }
+    },
+    scopes: {
+      todosOsRegistros: {
+        where: {}
+      }
+    }
   });
   return Pessoa;
 };
