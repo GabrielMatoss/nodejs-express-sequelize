555129c1b1fcf925177676e28cf87f92dff79839
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-02-16 12:00:53 -0300

    feat(models): adiciona validações nos campos de cpf, email e nome

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 'use strict';
+const isCpfValido = require('../../utils/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -16,17 +18,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'o campo nome deve ter no mínimo 3 caracteres'
+        }
+      }
+    },
     email: {
       type: DataTypes.STRING,
       validate: {
         isEmail: {
           args: true,
-          msg: 'formato de email inválido'
+          msg: 'formato do email inválido'
         }
       }
     },
-    cpf: DataTypes.STRING,
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if (!isCpfValido(cpf)) throw new Error('numero de CPF inválido');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
