c3e2e1c745a03f92450f5ba531ef367724f8aa53
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-02-19 09:40:17 -0300

    refactor(controllers): utilizando objeto where e modificando nomes de funções

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,3 +1,4 @@
+const converteIds = require('../utils/conversorDeStringHelper.js');
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
@@ -28,6 +29,23 @@ class Controller {
     }
   }
 
+  async pegaUm(req, res) {
+    const { ...params } = req.params;
+    const where = converteIds(params);
+
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistro(where);
+      
+      if(!umRegistro) {
+        return res.status(400).json({message: 'Registro não foi encontrado'});
+      }
+
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });    
+    }
+  }
+
   async criaNovo(req, res) {
     const dadosParaCriacao = req.body;
 
@@ -40,14 +58,14 @@ class Controller {
   }
 
   async atualiza(req, res) {
-    const { id } = req.params;
+    const { ...params } = req.params;
     const dadosAtualizados = req.body;
+    const where = converteIds(params);
 
     try {
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
-      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, where);
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(where.id);
       
-
       if (!foiAtualizado && !umRegistro) {
         return res.status(400).json({ message: 'Registro não foi atualizado' });
       }
@@ -59,10 +77,11 @@ class Controller {
   }
 
   async exclui(req, res) {
-    const { id } = req.params;
+    const { ...params } = req.params;
+    const where = converteIds(params);
     try {
-      await this.entidadeService.excluiRegistro(Number(id));
-      return res.status(200).json({ mensagem: `id ${id} deletado` });
+      await this.entidadeService.excluiRegistro(where);
+      return res.status(200).json({ mensagem: `id ${where.id} deletado` });
       
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -9,19 +9,19 @@ class PessoaController extends Controller {
   }
 
   async pegaMatriculasAtivas(req, res) {
-    const { estudanteId } = req.params;
+    const { estudante_id } = req.params;
     try {
-      const listaMatriculas = await pessoaServices.pegaMatriculasAtivasPorEstudante(Number(estudanteId));
+      const listaMatriculas = await pessoaServices.pegaMatriculasAtivasPorEstudante(Number(estudante_id));
       return res.status(200).json(listaMatriculas);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
     }
   }
   
-  async pegaTodasMatriculas(req, res) {
-    const { estudanteId } = req.params;
+  async pegaTodasAsMatriculas(req, res) {
+    const { estudante_id } = req.params;
     try {
-      const listaMatriculas = await pessoaServices.pegaTodasAsMatriculasPorEstudante(Number(estudanteId));
+      const listaMatriculas = await pessoaServices.pegaTodasAsMatriculasPorEstudante(Number(estudante_id));
       return res.status(200).json(listaMatriculas);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
